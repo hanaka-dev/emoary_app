6ed5ebed19c58ae4c96a128e59f837e83cf8cb81
hanaka-dev <4623102@ed.tus.ac.jp>
2025-08-29 19:19:45 +0900

    Modify host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  host = "https://emoary_app.onrender.com"
+  host = "https://emoary-app.onrender.com"
   config.action_mailer.default_url_options = {  host: host  }
   ActionMailer::Base.smtp_settings = {
     :port           =>   587,
